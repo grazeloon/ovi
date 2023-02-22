676e8d1254e186796e252502f7cfa1896f4fa47d
GrazeLoon <fou666parker@gmail.com>
2023-02-23 00:26:16 +0530

    config edit

--- config/site.config.js
+++ config/site.config.js
@@ -44,7 +44,7 @@ module.exports = {
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: [
     "/volume_block/public/root/MEDIA",
-    "/volume_block/public/root/MEDIA/protected_0",
+    // "/volume_block/public/root/MEDIA/protected_0",
   ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
